4924245bac832e4578ae5165b1853505ba459b4a
Eric Lau <ericglau@outlook.com>
2022-12-06 14:09:42 -0500

    Update comments

--- contracts/RBACBeacon.sol
+++ contracts/RBACBeacon.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-// This contract is provided as an example of an upgradeable beacon with role-based access control.
+
+// This contract is provided as an example only.
 
 pragma solidity ^0.8.0;
 
